c6d42aa37ef4c76e604c5569a8be44bc51cd7b9a
Joshua Weaver <weaverjw@my.lanecc.edu>
2023-07-19 11:01:59 -0400

    Added Timer class with functions that start the timer, reset the timer, and update the timer as it is running

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+import time
 
 # This is a program that allows users to track their study time using the Pomodoro technique
 
@@ -13,11 +14,11 @@ class App(tk.Tk):
 
         # Create a container frame for the two frames
         container = tk.Frame(self)
-        container.pack(fill = "both", expand = True)
+        container.pack(fill="both", expand = True)
 
         # Place clock and session data in container frame
-        clock = Clock(container, controller = self)
-        sessionData = SessionData(container, controller = self)
+        clock = Clock(container, controller=self)
+        sessionData = SessionData(container, controller=self)
         timer = tk.Frame(clock, height=150, width=250)
         timer.place(in_ = clock, anchor="center", relx=.5, rely=.5)
 
@@ -34,25 +35,77 @@ class App(tk.Tk):
         clock.grid(row=0, column=0, sticky="nsew")
         sessionData.grid(row=0, column=1, sticky="nsew")
 
-        # Place labels for Clock and SessionData frames
+
+        # Place labels for Clock, SessionData, and Timer frames
         label = tk.Label(clock, text ="Study Timer", bg = "black",fg ="white", font = ("Terminal",20))
-        label.pack(padx = 5, pady = 5)
+        label.pack(padx=5, pady=5)
+        self.timerLabel = tk.Label(timer, text = "25:00", bg ="black", fg ="white", font = ("Terminal", 30))
+        self.timerLabel.pack()
         sessionLabel = tk.Label(sessionData, text ="Session Data", bg ="black", fg="white", font = ("Terminal", 20))
-        sessionLabel.pack(padx= 5, pady = 5)
-
-        # Button to start timer, placed at bottom of Clock frame
-        button1 = tk.Button(clock, text = "START", font = ("Terminal", 14))
-        button1.pack(side ="bottom", padx=20, pady=140)
+        sessionLabel.pack()
 
 
+        # calls the Timer class
+        self.timer = Timer(self.timerLabel)
 
-# initializes frame containing the timer
+        # Button to start the timer
+        button1 = tk.Button(clock, text="START", bg="black", fg="white", font=("Terminal", 14), command=self.timer.startTimer)
+        button1.pack(side="bottom", padx=20, pady=140)
+        button2 = tk.Button(clock, text="RESET", bg="black", fg ="white", font=("Terminal", 14), command=self.timer.resetTimer)
+        button2.pack(side="bottom", padx=20, pady=80)
+
+
+# Timer class that starts and resets the timer
+class Timer:
+
+    # initializes the timer class
+    def __init__(self, label):
+        self.label = label
+        self.isRunning = False
+
+
+
+    #starts the timer
+    def startTimer(self):
+        if not self.isRunning:
+            self.isRunning = True
+            self.endTime = time.time() + (25 * 60)
+            self.updateTimer()
+
+    #resets the timer
+    def resetTimer(self):
+        if self.isRunning:
+            self.isRunning = False
+
+
+    # updates the timer as it is running
+    def updateTimer(self):
+        if self.isRunning:
+            remaining_time = int(self.endTime - time.time())
+            if remaining_time <= 0:
+                self.label.config(text="00:00")
+                self.isRunning = False
+            else:
+                minutes = remaining_time // 60
+                seconds = remaining_time % 60
+                self.label.config(text=f"{minutes:02d}:{seconds:02d}")
+                self.label.after(1000, self.updateTimer)
+
+
+
+
+
+
+
+
 class Clock(tk.Frame):
     # initializes Clock frame as a child of parent container
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
         self.controller = controller
         self.configure(background="forest green")
+
+
 
 
 # initializes frame containing session data
